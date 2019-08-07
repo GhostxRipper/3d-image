6e7f8a2fb1a5a96a9ea2c5cbca6500692ab3ca67
Yann Amsellem <yann_ams@icloud.com>
2019-08-07 12:36:20 +0200

    Updates

---
+++ precache-manifest.a4b1a2a2fcc9a80631eee6d457523ea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3dfd5df56e93d7354ee9b52f562046",
+    "url": "/3d-image/index.html"
+  },
+  {
+    "revision": "bc3c25516bb8f7274420",
+    "url": "/3d-image/static/css/main.60f48174.chunk.css"
+  },
+  {
+    "revision": "f6ad3b6af54baa6c2289",
+    "url": "/3d-image/static/js/2.9a4a5f41.chunk.js"
+  },
+  {
+    "revision": "bc3c25516bb8f7274420",
+    "url": "/3d-image/static/js/main.7a481112.chunk.js"
+  },
+  {
+    "revision": "2153b2e0efab2d499a7d",
+    "url": "/3d-image/static/js/runtime~main.18d67283.js"
+  }
+]);
