fd7cc2c95d35903e63615130aee46675642d5bb9
Yann Amsellem <yann_ams@icloud.com>
2019-08-07 12:09:56 +0200

    first commit: bootstrap + implementation of react-spring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react'
+import { animated, useSpring } from 'react-spring'
+import { useGesture } from 'react-use-gesture'
+
+import './card.css'
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 10,
+  (x - window.innerWidth / 2) / 10,
+  1.1,
+]
+
+const trans = (rx, ry, rs) => [
+  'perspective(600px)',
+  `rotateX(${rx}deg)`,
+  `rotateY(${ry}deg)`,
+  `scale(${rs})`,
+].join(' ')
 
 function App() {
+  const cardRef = useRef()
+
+  const [{ rxrys }, set] = useSpring(() => ({
+    rxrys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }))
+
+  const bind = useGesture(
+    {
+      onMove: ({ xy }) => set({ rxrys: calc(...xy) }),
+      onHover: ({ hovering }) => !hovering && set({ rxrys: [0, 0, 1] }),
+    },
+    {
+      domTarget: cardRef,
+    },
+  )
+
+  useEffect(bind, [bind])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <animated.div
+      ref={cardRef}
+      className="card"
+      style={{ transform: rxrys.interpolate(trans) }}
+    />
+  )
 }
 
-export default App;
+export default App
